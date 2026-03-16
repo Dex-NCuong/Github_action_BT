d2a585c5da436358ba7f990b1786b61554c6b451
Dex-NCuong <daonhatcuong2004@gmail.com>
2026-03-16 14:50:12 +0700

    first commit

---
+++ src/main/java/com/example/DaoNhatCuong/DaoNhatCuongApplication.java
@@ -0,0 +1,13 @@
+package com.example.DaoNhatCuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoNhatCuongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoNhatCuongApplication.class, args);
+	}
+
+}
